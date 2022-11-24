0fa043a478b48e15d8812a4e895742735628930d
Ukejecole <Ukejecole@gmail.com>
2022-11-23 21:29:20 -0500

    No errors

--- Chamber/Script/script.js
+++ Chamber/Script/script.js
@@ -2,27 +2,61 @@ const navBtn = document.querySelector("#nav-btn");
 const navEl = document.querySelector("nav");
 const dateEl = document.querySelector("#header-date");
 const footerEl = document.querySelector("#footer-year")
-const modEl = document.querySelector(".last-modification")
+const modEl = document.querySelector(".last-modification");
+const banner = document.querySelector(".banner");
+const visitString = document.querySelector("#visit-string");
+const hiddenInput = document.querySelector("#curtime");
+const removeBannerbtn = document.querySelector("#hex");
+const curDate = new Date();
 
+
+// Last Visit
+let count = 1;
+let dayText;
+const today = Math.floor(curDate/8.64e7);
+const formerday = Number(localStorage.getItem("lastDay"));
+const daydiff = today - formerday
+
+dayText = `${Math.round(daydiff)} days ago | Visit count:${localStorage.getItem("count")}`
+visitString.textContent = dayText;
+
+
+
+
+const now = Math.floor(curDate/8.64e7);
+localStorage.setItem("lastDay", now);
+count = localStorage.getItem("count");
+count++
+localStorage.setItem("count", count);
+
+
+
+// Toggle Navigation for mobile view
 navBtn.addEventListener("click", function(){
     navEl.classList.toggle("open")
     navBtn.classList.toggle("open")
 });
 
-const curDate = new Date()
+
+// Date
 const curDay = function(){
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     return days[curDate.getDay()]}
 const curDayofMonth = curDate.getDate()
 const curMonth = function(){
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October","November", "December"]
-    return months[curDate.getMonth()]}
-const curYear = curDate.getFullYear()
+    return months[curDate.getMonth()]};
+const curYear = curDate.getFullYear();
+const curMinute = curDate.getMinutes();
+const curSecond = curDate.getSeconds();
+const curHour = curDate.getHours;
 
 dateEl.innerHTML = `${curDay()}, ${curDayofMonth} ${curMonth()} ${curYear}`;
 
-footerEl.innerHTML = `&copy${curYear} Inland Valley Chamber`
+footerEl.innerHTML = `&copy${curYear} Island of Victoria Chamber`;
 
+
+// Last modified
 const lastModifiedDate = new Date(document.lastModified)
 
 let modYear = lastModifiedDate.getFullYear();
@@ -32,30 +66,18 @@ let modminute = lastModifiedDate.getMinutes();
 let modsecond = lastModifiedDate.getSeconds()
 let modhour = lastModifiedDate.getHours(); 
 
-modEl.innerHTML = `Last Updated: ${modMonth}/${modDay}/${modYear} ${modhour}:${modminute}:${modsecond}`
+modEl.innerHTML = `Last Updated: ${modMonth}/${modDay}/${modYear} ${modhour}:${modminute}:${modsecond}`;
 
-// set active
 
-function active() {
-    switch (document.title) {
-      case "Inland Chamber of Commerce":
-        document.querySelector("#home").classList.add("active");
-        break;
-  
-      case "Inland Chamber of Commerce - Discover":
-        document.querySelector("#discover").classList.add("active");
-        break;
-  
-      case "Inland Chamber of Commerce - Join":
-        document.querySelector("#join").classList.add("active");
-        break;
-  
-      case "Inland Chamber of Commerce - Directory":
-        document.querySelector("#directory").classList.add("active");
-        break;
-  
-      case "Inland Chamber of Commerce - Contact":
-        document.querySelector("#contact").classList.add("active");
-        break;
-    }
-  }
+// Banner
+if(curDay() == "Monday" || curDay() == "Tuesday"){
+    banner.classList.remove("hide");
+}else{
+    banner.style.display = "none";
+};
+
+
+// Remove Banner
+removeBannerbtn.addEventListener("click", function(){
+    banner.style.display = "none";
+});
