b2bf3961feee82923ebf577e8d910b2183ce2804
Ukejecole <Ukejecole@gmail.com>
2022-11-26 17:47:19 -0500

    done

--- Chamber/Script/script.js
+++ Chamber/Script/script.js
@@ -6,39 +6,38 @@ const modEl = document.querySelector(".last-modification");
 const banner = document.querySelector(".banner");
 const visitString = document.querySelector("#visit-string");
 const hiddenInput = document.querySelector("#curtime");
-const removeBannerbtn = document.querySelector("#hex");
 const curDate = new Date();
 
 
-// Last Visit
 let count = 1;
 let dayText;
 const today = Math.floor(curDate/8.64e7);
 const formerday = Number(localStorage.getItem("lastDay"));
 const daydiff = today - formerday
-
-dayText = `${Math.round(daydiff)} days ago | Visit count:${localStorage.getItem("count")}`
+if(daydiff > 1){
+    dayText = `${Math.round(daydiff)} days ago | Visit count:${localStorage.getItem("count")}`
+}else{
+    dayText = `This is your first day visiting | Visit count:${localStorage.getItem("count")}`;
+}
 visitString.textContent = dayText;
 
 
 
 
-const now = Math.floor(curDate/8.64e7);
-localStorage.setItem("lastDay", now);
+const now = Math.floor(curDate/8.64e7)
+localStorage.setItem("lastDay", now)
 count = localStorage.getItem("count");
 count++
 localStorage.setItem("count", count);
 
 
 
-// Toggle Navigation for mobile view
+
 navBtn.addEventListener("click", function(){
     navEl.classList.toggle("open")
     navBtn.classList.toggle("open")
 });
 
-
-// Date
 const curDay = function(){
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     return days[curDate.getDay()]}
@@ -53,10 +52,8 @@ const curHour = curDate.getHours;
 
 dateEl.innerHTML = `${curDay()}, ${curDayofMonth} ${curMonth()} ${curYear}`;
 
-footerEl.innerHTML = `&copy${curYear} Island of Victoria Chamber`;
+footerEl.innerHTML = `&copy${curYear} MainLand Valley Chamber`
 
-
-// Last modified
 const lastModifiedDate = new Date(document.lastModified)
 
 let modYear = lastModifiedDate.getFullYear();
@@ -66,18 +63,10 @@ let modminute = lastModifiedDate.getMinutes();
 let modsecond = lastModifiedDate.getSeconds()
 let modhour = lastModifiedDate.getHours(); 
 
-modEl.innerHTML = `Last Updated: ${modMonth}/${modDay}/${modYear} ${modhour}:${modminute}:${modsecond}`;
+modEl.innerHTML = `Last Updated: ${modMonth}/${modDay}/${modYear} ${modhour}:${modminute}:${modsecond}`
 
-
-// Banner
 if(curDay() == "Monday" || curDay() == "Tuesday"){
-    banner.classList.remove("hide");
+    banner.classList.remove("hide")
 }else{
-    banner.style.display = "none";
-};
-
-
-// Remove Banner
-removeBannerbtn.addEventListener("click", function(){
-    banner.style.display = "none";
-});
+    banner.classList.add("hide")
+}
